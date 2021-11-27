fa19f5830b3d92328215bd66cb4f7e8fb64d0aa5
Andreas Rohleder <andreas@rohleder.cc>
2021-11-27 16:59:11 +0100

    Removed non existant tests from build.bat

--- build.bat
+++ build.bat
@@ -26,13 +26,3 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 rem msbuild /p:Configuration=Release /p:Platform="Any CPU" documentation.shfbproj
 rem if %errorlevel% neq 0 exit /b %errorlevel%
 
-vstest.console Tests\bin\Release\net5.0\Test.dll
-vstest.console Tests\bin\Release\netcoreapp2.1\Test.dll
-vstest.console Tests\bin\Release\netcoreapp3.1\Test.dll
-vstest.console Tests\bin\Release\net20\Test.exe
-vstest.console Tests\bin\Release\net35\Test.exe
-vstest.console Tests\bin\Release\net40\Test.exe
-vstest.console Tests\bin\Release\net45\Test.exe
-vstest.console Tests\bin\Release\net46\Test.exe
-vstest.console Tests\bin\Release\net47\Test.exe
-vstest.console Tests\bin\Release\net48\Test.exe
