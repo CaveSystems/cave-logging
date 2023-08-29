32ea7fc2095d08e118bf4dac6583c74381d8bd9e
Andreas Rohleder <39777727+Dingsd4@users.noreply.github.com>
2023-08-29 18:28:50 +0200

    Removed Debugmessages from LoggerFlushTest

--- Tests/LoggerTest.cs
+++ Tests/LoggerTest.cs
@@ -13,11 +13,12 @@ namespace Tests;
 [TestFixture]
 class LoggerTest
 {
+    #region Public Methods
 
     [Test]
-    public void LogerFlushTest()
+    public void LoggerFlushTest()
     {
-        Logger.LogToDebug = true;
+        Logger.LogToDebug = Logger.LogToTrace = false;
         for (int i = 0; i < 100; i++)
         {
             var col = new LogCollector();
@@ -25,7 +26,7 @@ class LoggerTest
             col.LateMessageThreshold = -1;
             col.LateMessageMilliseconds = -1;
             col.MaximumItemCount = -1;
-            Parallel.For(0, 1000, n => new Logger().Info($"Test {n}"));
+            Parallel.For(0, 1000, n => new Logger().Info($"Test {i}.{n}"));
             Logger.Flush();
             var count = col.ItemCount;
             var items = col.ToArray().Select(l => int.Parse(l.Content.ToString().Substring(5))).ToList();
@@ -34,4 +35,6 @@ class LoggerTest
             Assert.AreEqual(1000, items.Count);
         }
     }
+
+    #endregion Public Methods
 }
