89d54aea305a382ca9c22f5751b9fbc9c54935e6
Andreas Rohleder <39777727+Dingsd4@users.noreply.github.com>
2024-01-28 13:49:26 +0100

    Implemented support for Microsoft.Extensions.Logging via new package (#7)

---
+++ Cave.Logging.Microsoft/CaveLoggerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cave.Logging;
+
+/// <summary>Provides a <see cref="Microsoft.Extensions.Logging.ILoggerFactory"/> implementation using cave logging.</summary>
+public class CaveLoggerFactory : Microsoft.Extensions.Logging.ILoggerFactory
+{
+    #region Public Properties
+
+    /// <summary>Gets all providers this factory uses.</summary>
+    public IEnumerable<Microsoft.Extensions.Logging.ILoggerProvider> Providers { get; } = new[] { new CaveLoggerProvider() };
+
+    #endregion Public Properties
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public void AddProvider(Microsoft.Extensions.Logging.ILoggerProvider provider) => throw new NotSupportedException();
+
+    /// <inheritdoc/>
+    public Microsoft.Extensions.Logging.ILogger CreateLogger(string categoryName) => Providers.First().CreateLogger(categoryName);
+
+    /// <inheritdoc/>
+    public void Dispose() { }
+
+    #endregion Public Methods
+}
